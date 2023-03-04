9c2d765a824881e24bdc88a8c15d6359a9856512
Tarpan <tarpanrai07@gmail.com>
2023-03-04 17:13:05 +0000

    Solutions

---
+++ 8 Kyu/DNAToRNAConversion.py
@@ -0,0 +1,6 @@
+# Replace T in DNA to U in RNA
+
+
+
+def dna_to_rna(dna):
+    return dna.replace('T', 'U')
---
+++ 8 Kyu/ReduceButGrow.py
@@ -0,0 +1,8 @@
+# Given a non-empty array of integers, return the result of
+# multiplying the values together in order.
+
+import math
+
+
+def grow(arr):
+    return math.prod(arr)
